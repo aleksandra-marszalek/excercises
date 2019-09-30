5795f5c68fe612c25ffc2947023c66385685f396
Aleksandra Marszalek <Aleksandra.Marszalek@oddschecker.com>
2019-09-30 09:49:27 +0100

    Change structure

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,33 +6,11 @@ import java.util.regex.*;
 
 public class Main {
 
-        static int B;
-        static int H;
-        static boolean flag;
+
+    public static void main(String[] args) {
 
 
-        static{
-            Scanner scan = new Scanner(System.in);
-            B = scan.nextInt();
-            scan.nextLine();
-            H = scan.nextInt();
-            scan.close();
-            if (B <= 0 || H <= 0){
-                flag = false;
-                System.out.println("java.lang.Exception: Breadth and height must be positive");
-            } else {
-                flag = true;
-            }
-        }
+        System.out.println(FirstMethod.helloName("Oddschecker"));
+    }
 
-
-        public static void main(String[] args){
-            if(flag){
-                int area=B*H;
-                System.out.println("I'm here'");
-                System.out.print(area);
-            }
-
-        }//end of main
-    //end of class
 }
