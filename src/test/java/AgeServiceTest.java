3575f0efa924d5ee04ef130c07c8bb7cb5f84216
Harry Clayton <hcl02@sbgml02900.sbg.skybettingandgaming.com>
2019-09-30 14:46:33 +0100

    updated solution 2

---
+++ src/test/java/AgeServiceTest.java
@@ -0,0 +1,25 @@
+import mocking_excercises.model.Person;
+import mocking_excercises.service.AgeService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import static org.junit.Assert.*;
+
+@RunWith(JUnit4.class)
+public class AgeServiceTest {
+    AgeService age = new AgeService();
+
+    @Test
+    public void testUnderage() {
+        Person under = new Person("testName1", 15);
+        assertEquals("testName1 is underage", age.printAge(under));
+    }
+
+    @Test
+    public void testAnAdult() {
+        Person over = new Person("testName2", 21);
+        assertEquals("testName2 is an adult", age.printAge(over));
+    }
+
+}
