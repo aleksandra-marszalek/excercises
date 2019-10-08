2f4090a6fccb39f5a80b73586af3fdde34c2c591
Harry Clayton <Harry.Clayton@oddschecker.com>
2019-10-08 16:43:26 +0100

    fixed some previous tests so that all pass

--- src/main/java/list_excercises/service/Compare.java
+++ src/main/java/list_excercises/service/Compare.java
@@ -19,18 +19,18 @@ public class Compare {
         if (len1 >= len2) {
             for (int i=0; i < len2; i++) {
                 if (arr1.get(i).equals(arr2.get(i))) {
-                    trues =+ 1.0;
+                    trues += 1.0;
                 } else {
-                    falses =+ 1.0;
+                    falses += 1.0;
                 }
             }
             perc = (trues * 100) / len1;
         } else {
             for (int j=0; j < len1; j++) {
                 if (arr1.get(j).equals(arr2.get(j))) {
-                    trues =+ 1.0;
+                    trues += 1.0;
                 } else {
-                    falses =+ 1.0;
+                    falses += 1.0;
                 }
             }
             perc = (trues * 100) / len2;
